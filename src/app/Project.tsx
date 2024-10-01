24fe364ff3203674bcc60bcbea0a0ea29b276a33
nich1 <nichtaylor516@gmail.com>
2024-10-01 14:15:37 -0500

    Fixing some bugs

--- src/app/Project.tsx
+++ src/app/Project.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import SkillCard from './SkillCard'
+import Image from 'next/image';
+import SkillCard from './SkillCard';
 
 interface ProjectProp {
     title: string;
@@ -7,23 +7,24 @@ interface ProjectProp {
     description: string;
     src: string;
     alt: string;
-  }
+}
 
-export default function Project({alt, title, caption, description, src}:CardProps) {
+// Use ProjectProp here instead of CardProps
+export default function Project({ alt, title, caption, description, src }: ProjectProp) {
     return (
         <div className='w-full flex justify-center pl-20'>
-                            <Image  className=''    
-                                    src={src}
-                                    width={200}
-                                    height={100}
-                                    alt={alt}
-                                />
-                            <div className='pl-4 '>
-                                <div className="underline underline-offset-8 pb-2  decoration-red-600 text-left text-5xl">{title}</div> 
-                                <div className='text-3xl'>{caption}</div>
-                                <div className="text-2xl text-neutral-600">{description}</div>
-                                
-                            </div>
+            <Image
+                className=''
+                src={src}
+                width={200}
+                height={100}
+                alt={alt}
+            />
+            <div className='pl-4'>
+                <div className="underline underline-offset-8 pb-2 decoration-red-600 text-left text-5xl">{title}</div>
+                <div className='text-3xl'>{caption}</div>
+                <div className="text-2xl text-neutral-600">{description}</div>
             </div>
-    )
+        </div>
+    );
 }
