92d6f0f7e55faae30f32e573c64ec87b4581407c
nich1 <91976504+nich1@users.noreply.github.com>
2024-10-01 18:21:14 -0500

    Changed icon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import AboutMe from "./AboutMe";
 import Education from "./Education";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Nick\'s Portfolio',
+  icons: {
+    icon: "appicon.png"
+  }
+}
+
 export default function Home() {
   return (
     <main className="!scroll-smooth">
